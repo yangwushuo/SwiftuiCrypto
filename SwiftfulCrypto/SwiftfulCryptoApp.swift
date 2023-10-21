8965274a026862245f71a220a5a1f2742c82ebec
jasonyang <51365593+yangwushuo@users.noreply.github.com>
2023-10-21 15:09:00 +0800

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Jason on 2023/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
